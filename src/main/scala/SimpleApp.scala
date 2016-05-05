10331b4910cfa7d7bbbd1222b5c3eb3ead233d75
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-05-05 17:27:56 +0000

    package with sbt

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -8,8 +8,8 @@ object SimpleApp {
         val conf = new SparkConf().setAppName("Simple Application")
         val sc = new SparkContext(conf)
         val textFile = sc.textFile(README_FILE, 2).cache()
-        val numAs = logData.filter(line => line.contains("a")).count()
-        val numBs = logData.filter(line => line.contains("b")).count()
+        val numAs = textFile.filter(line => line.contains("a")).count()
+        val numBs = textFile.filter(line => line.contains("b")).count()
         println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
     }
 }
